9e8b7934a19008d0a18b3ab614a4e3c0b3023fb3
nicerjs23 <nicerjs23@gmail.com>
2025-08-17 15:59:16 +0900

    Feat:Refactori경로 수정

--- src/main/java/com/example/sulmuro_app/controller/chat/ChatController.java
+++ src/main/java/com/example/sulmuro_app/controller/chat/ChatController.java
@@ -10,7 +10,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 
 @RequiredArgsConstructor
-@RequestMapping("/api/chat")
+@RequestMapping("/api/v1/chat")
 @RestController
 public class ChatController {
 
