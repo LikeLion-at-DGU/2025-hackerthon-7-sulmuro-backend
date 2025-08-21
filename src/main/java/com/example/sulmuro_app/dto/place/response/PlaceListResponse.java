a6deb81b8666378b2c9b8203a5bdb11a16b799f8
hyunwoo <dlagusdn0218@gmail.com>
2025-08-21 22:36:09 +0900

    refactor: PlaceListResponse Dto location 추가

--- src/main/java/com/example/sulmuro_app/dto/place/response/PlaceListResponse.java
+++ src/main/java/com/example/sulmuro_app/dto/place/response/PlaceListResponse.java
@@ -1,5 +1,6 @@
 package com.example.sulmuro_app.dto.place.response;
 
+import com.example.sulmuro_app.domain.bin.Location;
 import com.example.sulmuro_app.domain.place.Place;
 import com.example.sulmuro_app.domain.bin.PlaceCategory;
 import lombok.Getter;
@@ -13,6 +14,7 @@ public class PlaceListResponse {
     private BigDecimal lat;
     private BigDecimal lng;
     private String address;
+    private Location location;
     @com.fasterxml.jackson.annotation.JsonProperty("cover_image")
     private String coverImage;
 
@@ -22,6 +24,7 @@ public class PlaceListResponse {
         this.category = p.getCategory();
         this.lat = p.getLat();
         this.lng = p.getLng();
+        this.location = p.getLocation();
         this.address = p.getAddress();
         this.coverImage = coverImage;
     }
